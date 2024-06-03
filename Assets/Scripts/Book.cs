d82ec7f0c13d330fd3b9a9a783053ecdc350b685
user054 <whdtn0411@gmail.com>
2024-06-04 05:20:17 +0900

    set

--- Assets/Scripts/Book.cs
+++ Assets/Scripts/Book.cs
@@ -6,23 +6,43 @@ using UnityEngine;
 public class Book : MonoBehaviour
 {
     Animator anim;
+
+    Transform originalTransform;
+
+    [SerializeField] GameObject setUI;
     
     private void Start()
     {
         anim = GetComponent<Animator>();
+        originalTransform = transform;
     }
 
     public void OpenBook()
     {
         Debug.Log("Book Opened!");
+        if(OpenBookCoroutineCheck == null)
+        {
+            OpenBookCoroutineCheck = StartCoroutine(OpenBookCoroutine());
+        }
+           
+    }
+
+    Coroutine OpenBookCoroutineCheck = null;
+    IEnumerator OpenBookCoroutine()
+    {
         anim.SetTrigger("BookOpen");
-        
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
+
+        setUI.SetActive(true);
+        OpenBookCoroutineCheck = null;
+        yield break;
     }
 
     public void CloseBook()
     {
         Debug.Log("Book Closed!");
         anim.SetTrigger("BookClose");
+        setUI.SetActive(false);
 
     }
 
