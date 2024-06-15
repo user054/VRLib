3b3d623e7f79f0ba1331375901c8bf261d4c01a2
user054 <whdtn0411@gmail.com>
2024-06-15 14:00:01 +0900

    set

--- Assets/Scripts/BookRental.cs
+++ Assets/Scripts/BookRental.cs
@@ -1,6 +1,5 @@
 
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -1,10 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Robot : ButtonPressFunc
 {
+    UnityEngine.XR.InputDevice device;
+    [SerializeField] XRController controller;
     [SerializeField] GameObject []robotUI;
+    bool leftPressed = false;
+    bool rightPressed = false;
+    int imageIndex = 0;
+
+    private void Start()
+    {
+        device = controller.inputDevice;
+        StartCoroutine(BookRentalComplete());
+    }
+    private void Update()
+    {
+        InputHelpers.IsPressed(controller.inputDevice, InputHelpers.Button.SecondaryButton, out leftPressed);
+        InputHelpers.IsPressed(controller.inputDevice, InputHelpers.Button.PrimaryButton, out rightPressed);   
+    }
 
     public override void TriggerButtonPress()
     {
@@ -23,6 +39,59 @@ public class Robot : ButtonPressFunc
     void MainUiActive()
     {
         robotUI[0].SetActive(true);
+    }
+
+    IEnumerator BookRentalComplete()
+    {
+        while(true)
+        {
+            if(leftPressed == false && rightPressed ==false)
+            {
+                while(true)
+                {
+                    if (leftPressed == true)
+                    {
+                        robotUI[imageIndex].SetActive(false);
+                        imageIndex--;
+
+                        if(imageIndex > robotUI.Length - 1)
+                        {
+                            imageIndex = robotUI.Length - 1;
+                        }
+                        else if(imageIndex < 0)
+                        {
+                            imageIndex = 0;
+                        }
+
+                        robotUI[imageIndex].SetActive(true);
+
+                        //yield return new WaitForSeconds(1.0f);
+                        
+                    }
+                    else if(rightPressed == true)
+                    {
+                        robotUI[imageIndex].SetActive(false);
+                        imageIndex++;
+
+                        if(imageIndex > robotUI.Length - 1)
+                        {
+                            imageIndex = robotUI.Length - 1;
+                        }
+                        else if(imageIndex < 0)
+                        {
+                            imageIndex = 0;
+                        }
+
+                        robotUI[imageIndex].SetActive(true);
+
+                        //yield return new WaitForSeconds(1.0f);
+                    }
+                    yield return null;
+                }   
+            }
+
+            yield return null;
+        }
     }
 
 }
